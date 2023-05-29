0d3fb94910769c0396c86258514d5d4b91838a3c
Rana Ahmed <ranaete14@gmail.com>
2023-05-29 17:00:57 +0600

    problem1 completed

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,53 +1,106 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const Problem1 = () => {
 
     const [show, setShow] = useState('all');
+    const [tasks, setTasks] = useState([]);
+    const [name, setName] = useState('');
+    const [status, setStatus] = useState('active');
+    const [filter, setFilter] = useState('all');
 
-    const handleClick = (val) =>{
+    const handleClick = (val) => {
         setShow(val);
-    }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newTask = {
+            name: name,
+            status: status,
+        };
+        setTasks([...tasks, newTask]);
+        setName('');
+        setStatus('active');
+    };
+
+    const handleFilterChange = (e) => {
+        setFilter(e.target.value);
+    };
+
+    const filteredTasks = tasks.filter((task) => {
+        if (filter === 'active') {
+            return task.status === 'active';
+        } else if (filter === 'completed') {
+            return task.status === 'completed';
+        }
+        return true;
+    });
+
+    const sortedTasks = filteredTasks.sort((a, b) => {
+        if (a.status === 'active' && b.status !== 'active') {
+            return -1;
+        } else if (a.status !== 'active' && b.status === 'active') {
+            return 1;
+        } else if (a.status === 'completed' && b.status !== 'completed') {
+            return -1;
+        } else if (a.status !== 'completed' && b.status === 'completed') {
+            return 1;
+        }
+        return 0;
+    });
 
     return (
 
         <div className="container">
             <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
+                <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form className="row gy-2 gx-3 align-items-center mb-4" onSubmit={handleSubmit}>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input type="text" className="form-control" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input type="text" className="form-control" placeholder="Status" value={status} onChange={(e) => setStatus(e.target.value)} />
                         </div>
                         <div className="col-auto">
-                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <button type="submit" className="btn btn-primary">
+                                Submit
+                            </button>
                         </div>
                     </form>
                 </div>
                 <div className="col-8">
                     <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
+                            <button className={`nav-link ${show === 'all' && 'active'}`} type="button" onClick={() => handleClick('all')}>
+                                All
+                            </button>
                         </li>
                         <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
+                            <button className={`nav-link ${show === 'active' && 'active'}`} type="button" onClick={() => handleClick('active')}>
+                                Active
+                            </button>
                         </li>
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
+                            <button className={`nav-link ${show === 'completed' && 'active'}`} type="button" onClick={() => handleClick('completed')}>
+                                Completed
+                            </button>
                         </li>
                     </ul>
-                    <div className="tab-content"></div>
-                    <table className="table table-striped ">
+                    <table className="table table-striped">
                         <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
+                            <tr>
+                                <th>Name</th>
+                                <th>Status</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        
+                            {sortedTasks.map((task, index) => (
+                                <tr key={index}>
+                                    <td>{task.name}</td>
+                                    <td>{task.status}</td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
